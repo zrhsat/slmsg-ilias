2286115a176486890dbc6a1961e354c28c50bb34
schererm <mail@michaelscherer.ch>
2016-01-27 10:49:09 +0100

    added new comments to code

--- msg.php
+++ msg.php
@@ -1,6 +1,6 @@
 <?php
 $msgFolder = "html/";
-//grouping array to create group messages from groups.json
+//import grouping array from groups.json
 if(file_exists("groups.json")){
      $groups = json_decode(file_get_contents("groups.json"),true); 
 }
@@ -21,16 +21,16 @@ if( $_REQUEST["ref_id"] ) {
         }
     } 
 //outout module message if no group message is available    
-    else{
-        if(file_exists($msgFolder.$_REQUEST['ref_id'].".html")){
-            echo file_get_contents ( $msgFolder.$_REQUEST['ref_id'].".html");
-        }   
+    elseif(file_exists($msgFolder.$_REQUEST['ref_id'].".html")){
+        echo file_get_contents ( $msgFolder.$_REQUEST['ref_id'].".html");
     } 
+//output standard message if no specific message is available
+    elseif(file_exists($msgFolder."nomsg.html")){
+        echo file_get_contents ( $msgFolder."nomsg.html");
+    }
 }
 //do something if no ref_id is available
-else{
-    if(file_exists($msgFolder."nomsg.html")){
-        echo file_get_contents ( $msgFolder."nomsg.html");
-    } 
+elseif(file_exists($msgFolder."nomsg.html")){
+    echo file_get_contents ( $msgFolder."nomsg.html");
 } 
 ?>
