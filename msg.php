09f1d335da25522419dc802a335b47d7963284cf
schererm <mail@michaelscherer.ch>
2016-01-29 14:08:26 +0100

    code beautifying

--- msg.php
+++ msg.php
@@ -1,36 +1,60 @@
 <?php
 $msgFolder = "html/";
-//import grouping array from groups.json
-if(file_exists("groups.json")){
-     $groups = json_decode(file_get_contents("groups.json"),true); 
+
+// Import grouping array from groups.json
+
+if (file_exists("groups.json"))
+{
+     $groups = json_decode(file_get_contents("groups.json"), true); 
 }
-//get the whole request uri sent by js script by variable
-if( $_REQUEST["source"] ) {
+
+// Get the whole request uri sent by js script by variable
+
+if ($_REQUEST["source"])
+{
     //do something
 }
-//get the hostname sent by js script by variable
-if( $_REQUEST["host"] ) {
-    //do something
+
+// Get the hostname sent by js script by variable
+
+if ($_REQUEST["host"]) 
+{
+    // do something
 }
-//check if ref_id is available
-if( $_REQUEST["ref_id"] ) {
-//output group message if available    
-    if(array_key_exists( $_REQUEST['ref_id'] , $groups)){
-        if(file_exists($msgFolder.$groups[$_REQUEST['ref_id']].".html")){
-            echo file_get_contents ( $msgFolder.$groups[$_REQUEST['ref_id']].".html");
+
+// Check if ref_id is available
+
+if ($_REQUEST["ref_id"]) 
+{
+    // output group message if available    
+
+    if (array_key_exists($_REQUEST['ref_id'], $groups))
+    {
+        if (file_exists($msgFolder.$groups[$_REQUEST['ref_id']].".html"))
+        {
+            echo file_get_contents($msgFolder.$groups[$_REQUEST['ref_id']].".html");
         }
     } 
-//outout module message if no group message is available    
-    elseif(file_exists($msgFolder.$_REQUEST['ref_id'].".html")){
+    
+    // output module message if no group message is available    
+    
+    elseif (file_exists($msgFolder.$_REQUEST['ref_id'].".html"))
+    {
         echo file_get_contents ( $msgFolder.$_REQUEST['ref_id'].".html");
     } 
-//output standard message if no specific message is available
-    elseif(file_exists($msgFolder."nomsg.html")){
+    
+    // output standard message if no specific message is available
+    
+    elseif (file_exists($msgFolder."nomsg.html"))
+    {
         echo file_get_contents ( $msgFolder."nomsg.html");
     }
 }
+
 //do something if no ref_id is available
-elseif(file_exists($msgFolder."nomsg.html")){
-    echo file_get_contents ( $msgFolder."nomsg.html");
+
+elseif (file_exists($msgFolder."nomsg.html"))
+{
+    echo file_get_contents($msgFolder."nomsg.html");
 } 
 ?>
